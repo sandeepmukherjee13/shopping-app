8cc9b72279814dac1a62ada77042f4c07f26ec70
sandeepmukherjee <sandeepmukherjee12@gmail.com>
2023-12-04 21:33:59 +0530

    Common Modal

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,9 @@
 'use client';
 
+import { GlobalContext } from '@/context';
 import { adminNavOptions, navOptions } from '@/utils';
+import { useContext } from 'react';
+import CommonModal from '../CommonModal';
 
 const isAdminView = false;
 const isAuthUser = true;
@@ -9,13 +12,17 @@ const user = {
   role: 'admin'
 };
 
-function NavItems() {
+function NavItems({ isModalView = false }) {
   return (
     <div
-      className="items-center justify-between w-full md:flex md:w-auto"
+      className={`items-center justify-between w-full md:flex md:w-auto ${
+        isModalView ? '' : 'hidden'
+      }`}
       id="nav-items"
     >
-      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white">
+      <ul
+        className={`flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white ${isModalView}?"border-none":"border border-gray-100"`}
+      >
         {isAdminView
           ? adminNavOptions.map((item) => (
               <li
@@ -39,6 +46,8 @@ function NavItems() {
 }
 
 const Navbar = () => {
+  const { showNavModal, setShowNavModal } = useContext(GlobalContext);
+
   return (
     <>
       <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
@@ -113,7 +122,7 @@ const Navbar = () => {
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-sticky"
               aria-expanded="false"
-              // onClick={() => setShowNavModal(true)}
+              onClick={() => setShowNavModal(true)}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -134,6 +143,12 @@ const Navbar = () => {
           <NavItems />
         </div>
       </nav>
+      <CommonModal
+        show={showNavModal}
+        setShow={setShowNavModal}
+        showModalTitle={false}
+        mainContent={<NavItems isModalView={true} />}
+      />
     </>
   );
 };
--- src/context/index.js
+++ src/context/index.js
@@ -1,10 +1,15 @@
 'use client';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
-  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+  const [showNavModal, setShowNavModal] = useState(false);
+  return (
+    <GlobalContext.Provider value={{ showNavModal, setShowNavModal }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 }
 
 // setting up context - https://stackoverflow.com/questions/74311376/using-react-context-with-nextjs13-server-side-components
