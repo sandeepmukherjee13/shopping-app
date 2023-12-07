2984533ec983d2f3b36d49c9030a31bb890d99ea
sandeepmukherjee <sandeepmukherjee12@gmail.com>
2023-12-07 21:54:33 +0530

    Register Component

--- src/utils/index.js
+++ src/utils/index.js
@@ -38,3 +38,44 @@ export const adminNavOptions = [
     path: '/admin-view/add-product'
   }
 ];
+
+export const registrationFormControls = [
+  {
+    id: 'name',
+    type: 'text',
+    placeholder: 'Enter Your Name',
+    label: 'Name',
+    componentType: 'input'
+  },
+  {
+    id: 'email',
+    type: 'email',
+    placeholder: 'Enter Your Email',
+    label: 'Email',
+    componentType: 'input'
+  },
+  {
+    id: 'password',
+    type: 'password',
+    placeholder: 'Enter Your Password',
+    label: 'Password',
+    componentType: 'input'
+  },
+  {
+    id: 'role',
+    type: '',
+    placeholder: '',
+    label: 'Role',
+    componentType: 'select',
+    options: [
+      {
+        id: 'admin',
+        label: 'Admin'
+      },
+      {
+        id: 'customer',
+        label: 'Customer'
+      }
+    ]
+  }
+];
