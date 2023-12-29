1b65d614d9b19d120c866249d0039c555e91ee27
sandeepmukherjee <sandeepmukherjee12@gmail.com>
2023-12-29 21:43:38 +0530

    Notification Component

---
+++ src/components/Notification/index.js
@@ -0,0 +1,18 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Notification() {
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      pauseOnFocusLoss
+      draggable
+      theme="light"
+      rtl={false}
+    />
+  );
+}
