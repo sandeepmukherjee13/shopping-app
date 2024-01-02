c006f4e20c7de5f8285cb258754d16f5045dede6
sandeepmukherjee <sandeepmukherjee12@gmail.com>
2024-01-02 10:06:59 +0530

    AddNewProduct Component

--- src/app/admin-view/add-product/page.js
+++ src/app/admin-view/add-product/page.js
@@ -1,3 +1,45 @@
+'use client';
+
+import InputComponent from '@/components/FormElements/InputComponent';
+import SelectComponent from '@/components/FormElements/SelectComponent';
+import TileComponent from '@/components/FormElements/TileComponent';
+import { AvailableSizes, adminAddProductFormControls } from '@/utils';
+
 export default function AdminAddNewProduct() {
-  return <div>Admin View Add new product</div>;
+  function handleImage() {}
+  return (
+    <div className="w-full mt-5 mr-0 mb-0 ml-0 relative">
+      <div className="flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl relative">
+        <div className="w-full mt-6 mr-0 mb-0 ml-0 space-y-8">
+          <input
+            accept="image/*"
+            max="1000000"
+            type="file"
+            onChange={handleImage}
+          />
+          <div className="flex gap-2 flex-col">
+            <label>Available Sizes</label>
+            <TileComponent data={AvailableSizes} />
+          </div>
+          {adminAddProductFormControls.map((controlItem) =>
+            controlItem.componentType === 'input' ? (
+              <InputComponent
+                type={controlItem.type}
+                placeholder={controlItem.placeholder}
+                label={controlItem.label}
+              />
+            ) : controlItem.componentType === 'select' ? (
+              <SelectComponent
+                label={controlItem.label}
+                options={controlItem.options}
+              />
+            ) : null
+          )}
+          <button className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white font-medium uppercase tracking-wide">
+            Add Product
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
